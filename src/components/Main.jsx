c76494f954802355dbe8603e07a58aa8f899b646
Code25 <chingisenkhbaatar@gmail.com>
2024-02-09 15:45:14 +0100

    added difficulty to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/index.css';
-import CardItem from './CardItem';
+import React, { useState, useEffect } from "react";
+import "../styles/index.css";
+import CardItem from "./CardItem";
 
-const Main = () => {
+const Main = ({difficulty}) => {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
     async function fetchCards() {
-      const response = await fetch('/cards.json');
+      const response = await fetch("/cards.json");
       const data = await response.json();
       setCards(data);
     }
@@ -16,26 +16,39 @@ const Main = () => {
 
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1)); // Corrected random index calculation
+      const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
   }
-  
 
   // Shuffle cards on component mount
   useEffect(() => {
     setCards((prevCards) => shuffleArray([...prevCards]));
   }, []);
 
+  // function to spawn cards based on difficulty
+  function spawnCards(difficulty) {
+    let numCards = 0;
+    switch (difficulty) {
+      case "easy":
+        numCards = 4;
+        break;
+      case "intermediate":
+        numCards = 8;
+        break;
+      case "hard":
+        numCards = 16;
+        break;
+      default:
+        numCards = 4;
+    }
+    return cards.slice(0, numCards);
+  }
   return (
     <div className="main">
-      {cards.map((cards, index) => (
-        <CardItem
-          key={index}
-          image={cards.image}
-          title={cards.name} // Assuming your JSON has a "name" field for the card title
-        />
+      {spawnCards(difficulty).map((cards, index) => (
+        <CardItem key={index} image={cards.image} title={cards.name} />
       ))}
     </div>
   );

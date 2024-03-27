13d31b313058566c783e2a831c5da6490606d6d2
code25 <chingisenkhbaatar@gmail.com>
2024-03-27 18:41:01 +0100

    added logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,15 @@ import "../styles/Header.css";
 const Header = ({ score, highScore }) => {
   return (
     <div className="header">
-      <h1>Memory Game</h1>
+      <div className="flex self-center justify-center items-center ">
+        <img
+          src="/images/logo.png"
+          alt="Logo"
+          className="h-[250px] top-0 left-0 relative"
+        />
+
+        <h1 className="text-white text-4xl font-bold"> Memory Game</h1>
+      </div>
 
       <div className="eyeImg">.</div>
       <div className="scoreboard">

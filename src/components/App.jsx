1c2e8739876ed7db22ab7d63e94f24e4656874a4
code25 <chingisenkhbaatar@gmail.com>
2024-04-03 21:54:09 +0200

    added footer component to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import DifficultyPopup from "./DifficultyPopup";
 import Header from "./Header";
 import Main from "./Main";
 import "../styles/App.css";
-import 'tailwindcss/tailwind.css';
+import Footer from "./Footer";
+import "tailwindcss/tailwind.css";
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
@@ -24,19 +25,17 @@ function App() {
 
   return (
     <div className="App">
-
       <div className="title-background"></div>
 
-        <Header
-          score={score}
-          highScore={highScore}
-          setScore={setScore}
-          setHighScore={setHighScore}
-        />
-        <Main difficulty={difficulty} score={score} setScore={setScore} />
-        
-    
-      
+      <Header
+        score={score}
+        highScore={highScore}
+        setScore={setScore}
+        setHighScore={setHighScore}
+      />
+      <Main difficulty={difficulty} score={score} setScore={setScore} />
+
+      <Footer />
     </div>
   );
 }
